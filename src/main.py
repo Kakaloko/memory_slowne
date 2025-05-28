02c1d0935d8081397ce85add19160364c1a01e05
SzymonBawor <szymon.bawor05@gmail.com>
2025-05-28 22:57:52 +0200

    moja propozycja na dodanie trybu na czas

--- src/main.py
+++ src/main.py
@@ -30,3 +30,54 @@ for slowo in lista2:
     if slowo in lista_do_wyswietlenia:
         punkty += 1
 print(punkty)
+
+
+
+"""import random
+import time
+import os
+
+def tryb_gry_na_czas(lista_slow):
+    czas = int(input("Podaj czas potrzebny na zapamiętanie 10 słów: "))
+    lista_do_wyswietlenia = random.sample(lista_slow, 5)
+    print("Wylosowane słowa:", lista_do_wyswietlenia)
+    for i in range(czas, 0, -1):
+        print(f"\rPozostały czas {i} s do rozpoczęcia gry", end='', flush=True)
+        time.sleep(1)
+   # print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
+    print("\n" * 10)
+    lista_wpisana = input().split(" ")
+    punkty = 0
+    for slowo in lista_slow:
+        if slowo in lista_do_wyswietlenia:
+            punkty += 1
+    return punkty
+
+lista_latwych = ["laptop", "lampa", "telefon", "okno", "zegarek", "butelka", "szalik", "sok", "plecak", "mysz", "garnek", "długopis",
+"nożyczki", "marker", "masło", "ryż", "makaron", "poduszka", "klapki", "gitara", "jabłko", "portfel", "słoik", "czajnik"]
+
+def main():
+    p = tryb_gry_na_czas(lista_latwych)
+    print(p)
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+#pierwsza wersja:    
+lista_do_wyswietlenia = random.sample(lista_latwych, 5)
+print("Wylosowane słowa:", lista_do_wyswietlenia)
+
+for i in range(3, 0, -1):
+    print(f"\rZniknie za {i} sekund...", end='', flush=True)
+    time.sleep(1)
+
+print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
+print("\n" * 10)
+lista_wpisana = input().split(" ")
+punkty = 0
+for slowo in lista2:
+    if slowo in lista_do_wyswietlenia:
+        punkty += 1
+print(punkty)"""
