cd72a9cfbcb8cfe432c716239dff26d44e2ceefb
Hubert <h.bryki@gmail.com>
2025-05-28 21:18:16 +0200

    naprawione

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import random
 import time
 import os
-import csv
 
 # Lista słów
-with open("data/easy_level.csv", newline=" ") as csvfile:
-    list = csvfile.reader()
 lista = ["laptop", "lampa", "telefon", "okno", "zegarek", "butelka", "szalik", "sok", "plecak", "mysz"]
 
 # Losowanie 5 słów
@@ -18,6 +14,3 @@ time.sleep(10)
 
 # Wyczyść konsolę
 os.system('cls')  # Dla Windows
-=======
-print("Hello World")
->>>>>>> f9026a8e26d443dc1516e3c3169fda9d14a0dcf4
