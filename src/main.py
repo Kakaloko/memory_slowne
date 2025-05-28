13cb4b3dd4aaae3be78e053d6aab95e9048accee
Hubert <h.bryki@gmail.com>
2025-05-28 20:44:53 +0200

    test_gui

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from tkinter import *
+
+root = Tk()
+root.title("Memory slowne")
+root.geometry("1000x1000")
+
+lbl = Label(root, text = "Dziala?")
+lbl.grid()
+
+root.mainloop()
