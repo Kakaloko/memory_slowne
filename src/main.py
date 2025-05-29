01c99635ec20e28a6ade25a4507e0f68d432409b
SzymonBawor <szymon.bawor05@gmail.com>
2025-05-29 15:24:29 +0200

    dodanie_trybu_gry_na_ilość_i_statystka_błędów

--- src/main.py
+++ src/main.py
@@ -1,71 +1,105 @@
 import random
 import time
 import os
+#from asyncio.unix_events import can_use_pidfd
 
-tryb_gry = input("Wybierz tryb(easy, normal, hard)")
-
-plik = open("../data/"+ tryb_gry +"_level.csv","r")
-lista = plik.readline().split(",")
-plik.close()
-
-print("wybierz tryb gry: \n A <-- gra na czas \n B <-- gra na ilosc")
-
-Wybor = input("Wybierz tryb A lub B : ")
-if Wybor == "A":
-    liczba_s = int(input("Wybierz ile chcesz miec czasu na zadanie : "))
-    lista = ["laptop", "lampa", "telefon", "okno", "zegarek", "butelka", "szalik", "sok", "plecak", "mysz"]
-
-    lista_do_wyswietlenia = random.sample(lista, 5)
-    print("Wylosowane słowa:", lista_do_wyswietlenia)
-    for i in range(liczba_s, 0, -1):
-        print(f"\rZniknie za {i} sekund...", end='', flush=True)
-        time.sleep(1)
-
-# Czyszczenie linii i wiadomość końcowa
-print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
-print("\n" * 10)
-lista2 = input().split(" ")
-punkty = 0
-for slowo in lista2:
-    if slowo in lista_do_wyswietlenia:
-        punkty += 1
-print(punkty)
-
-
-
-"""import random
-import time
-import os
 
 def tryb_gry_na_czas(lista_slow):
     czas = int(input("Podaj czas potrzebny na zapamiętanie 10 słów: "))
     lista_do_wyswietlenia = random.sample(lista_slow, 5)
+    lista_do_wyswietlenia = [s.lower() for s in lista_do_wyswietlenia]
     print("Wylosowane słowa:", lista_do_wyswietlenia)
     for i in range(czas, 0, -1):
         print(f"\rPozostały czas {i} s do rozpoczęcia gry", end='', flush=True)
         time.sleep(1)
    # print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
-    print("\n" * 10)
-    lista_wpisana = input().split(" ")
+    print("\n" * 15)
+    lista_wpisana = [s.lower() for s in input("Wpisz zapamiętane słowa, oddzielone spacją: ").split()]
     punkty = 0
-    for slowo in lista_slow:
+    bledy = 0
+    for slowo in lista_wpisana:
         if slowo in lista_do_wyswietlenia:
             punkty += 1
-    return punkty
+        else:
+            bledy += 1
+    #punkty = sum(1 for slowo in lista_wpisana if slowo in lista_do_wyswietlenia) # inny sposób na zliczanie pkt
+
+    return punkty, bledy
+
+
+def tryb_gry_na_ilosc(lista_slow):
+    ilosc = int(input("Podaj ile słów chcesz zapamiętać w ciągu 10s: "))
+    lista_do_wyswietlenia = random.sample(lista_slow, ilosc)
+    print("Wylosowane słowa:", lista_do_wyswietlenia)
+    for i in range(10, 0, -1):
+        print(f"\rPozostały czas {i} s do rozpoczęcia gry", end='', flush=True)
+        time.sleep(1)
+    # print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
+    print("\n" * 15)
+    lista_wpisana = [s.lower() for s in input("Wpisz zapamiętane słowa, oddzielone spacją: ").split()]
+    punkty = 0
+    bledy = 0
+    for slowo in lista_wpisana:
+        if slowo in lista_do_wyswietlenia:
+            punkty += 1
+        else:
+            bledy += 1
+    # punkty = sum(1 for slowo in lista_wpisana if slowo in lista_do_wyswietlenia) # inny sposób na zliczanie pkt
+
+    return punkty, bledy
+
 
 lista_latwych = ["laptop", "lampa", "telefon", "okno", "zegarek", "butelka", "szalik", "sok", "plecak", "mysz", "garnek", "długopis",
-"nożyczki", "marker", "masło", "ryż", "makaron", "poduszka", "klapki", "gitara", "jabłko", "portfel", "słoik", "czajnik"]
+                 "nożyczki", "marker", "masło", "ryż", "makaron", "poduszka", "klapki", "gitara", "jabłko", "portfel", "słoik", "czajnik"]
+
+lista_srednich = ["komputer", "rowerzysta", "kalendarz", "dynastia", "przyjaciel", "ogródek", "śniadanie", "zapałki", "torbieli",
+                  "ekranowy", "domownik", "zegarowy", "kamizelka", "cukiernia", "kapelusz", "biblioteka", "wędkarstwo", "telefonia",
+                  "parasolka", "artysta", "narzędzia", "motylek", "podróżnik", "światło", "zeszytowy"]
+
+lista_trudnych = ["przeziębienie", "oświadczenie", "pożądliwość", "zatrzęsienie", "prześladowca", "żółciopędny", "przysięgający",
+                  "błyskotliwość", "współdziałanie", "niewyjaśniony", "półksiężyc", "świadomość", "złożoność", "przemyślenie",
+                  "dźwięczność", "łagodniejszy", "niezależność", "zaprzyjaźnić", "zażenowanie", "źreniczność", "nadziejość",
+                  "koleżeństwo", "uprzedzenie", "namiętność", "miękkość"]
+
 
 def main():
-    p = tryb_gry_na_czas(lista_latwych)
-    print(p)
+    lista =""
+    while True:
+        poziom_trudności = input("Wpisz poziom trudności (łatwy, średni, trudny): ")
+        match poziom_trudności:
+            case "łatwy":
+                lista = lista_latwych
+                break
+            case "średni":
+                lista = lista_srednich
+                break
+            case "trudny":
+                lista = lista_trudnych
+                break
+            case _:
+                print("Niepoprawny poziom trudności. Wpisz ponownie.")
+    tryb_gry = ""
+    punkty = 0
+    bledy = 0
+    while True:
+        tryb_gry = input("Wybierz tryb gry (na czas, na ilość): ")
+        match tryb_gry:
+            case "na czas":
+                punkty, bledy = tryb_gry_na_czas(lista)
+                break
+            case "na ilość":
+                punkty, bledy = tryb_gry_na_ilosc(lista)
+                break
+            case _:
+                print("Niepoprawny tryb gry. Wpisz ponownie.")
+    print("Liczba zdobytych punktów: ", punkty)
+    print("Liczba błędów: ", bledy)
 
 if __name__ == "__main__":
     main()
-    
-    
-    
-#pierwsza wersja:    
+
+#pierwsza wersja:
+"""
 lista_do_wyswietlenia = random.sample(lista_latwych, 5)
 print("Wylosowane słowa:", lista_do_wyswietlenia)
 
