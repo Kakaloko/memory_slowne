822163c731f1ff3b2454d9e2f92024203add63cc
Hubert <h.bryki@gmail.com>
2025-05-30 17:15:42 +0200

    menu

--- src/main.py
+++ src/main.py
@@ -1,10 +1,49 @@
-from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 
-root = Tk()
-root.title("Memory slowne")
-root.geometry("1000x1000")
+class Root(tk.Tk):
+    def __init__(self, text="Memory słowne"):
+        super().__init__()
+        self.title(text)
+        self.geometry("600x600")
+        self.configure(bg="#333446")
+       
+        self.menu_window()
+        
+        
+    def menu_window(self):
+        self.clear()
 
-lbl = Label(root, text = "Dziala?")
-lbl.grid()
+        self.frame = tk.Frame(self)
+        self.frame.configure(bg="#333446")
 
-root.mainloop()
+        play_button = tk.Button(self.frame, text="Graj", bg="#7F8CAA",borderwidth=0, command=self.game_window,)
+        play_button.pack()
+
+
+        statistics_button = tk.Button(self.frame, text="Statystyki",bg="#7F8CAA", command= self.statistics_window)
+        statistics_button.pack()
+       
+
+        option_button = tk.Button(self.frame, text="Opcje",bg="#7F8CAA", command=self.option_window)
+        option_button.pack()
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+
+    def statistics_window(self):
+        self.clear()
+
+
+    def game_window(self):
+        self.clear()
+
+    def option_window(self):
+        self.clear()
+
+    def clear(self):
+        for widget in self.winfo_children():
+            widget.destroy()
+
+if __name__ == "__main__":
+    root = Root()
+    root.mainloop()
