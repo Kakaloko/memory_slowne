df9d982e58b095620d3375c6ea8498598966a3a7
BartoszB755 <bartosz.brzuchacz@interia.pl>
2025-06-11 22:07:53 +0200

    main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,74 @@
-
 import random
 import time
 from operator import contains
+
+
+def tryb_gry_na_czas(lista_slow):
+    czas = int(input("Podaj czas potrzebny na zapamiętanie 10 słów: "))
+    lista_do_wyswietlenia = random.sample(lista_slow, 5)
+    lista_do_wyswietlenia = [s.lower() for s in lista_do_wyswietlenia]
+    print("Wylosowane słowa:", lista_do_wyswietlenia)
+    for i in range(czas, 0, -1):
+        print(f"\rPozostały czas {i} s do rozpoczęcia gry", end='', flush=True)
+        time.sleep(1)
+    print("\n" * 15)
+    lista_wpisana = [s.lower() for s in input("Wpisz zapamiętane słowa, oddzielone spacją: ").split()]
+    punkty = 0
+    bledy = 0
+    for slowo in lista_wpisana:
+        if slowo in lista_do_wyswietlenia:
+            punkty += 1
+        else:
+            bledy += 1
+    print("Koniec gry!")
+    return punkty, bledy
+
+def tryb_gry_na_ilosc(lista_slow):
+    ilosc = int(input("Podaj ile słów chcesz zapamiętać w ciągu 10s: "))
+    lista_do_wyswietlenia = random.sample(lista_slow, ilosc)
+    print("Wylosowane słowa:", lista_do_wyswietlenia)
+    for i in range(10, 0, -1):
+        print(f"\rPozostały czas {i} s do rozpoczęcia gry", end='', flush=True)
+        time.sleep(1)
+    print("\n" * 15)
+    lista_wpisana = [s.lower() for s in input("Wpisz zapamiętane słowa, oddzielone spacją: ").split()]
+    punkty = 0
+    bledy = 0
+    for slowo in lista_wpisana:
+        if slowo in lista_do_wyswietlenia:
+            punkty += 1
+        else:
+            bledy += 1
+    print("Koniec gry!")
+    return punkty, bledy
+
+def tryb_gry_do_pierwszego_bledu1(lista_slow):  # gra się kończy jeśli gracz poda mniejszą liczbę słów
+    punkty = 0
+    bledy = 0
+    i = 1
+    while True:
+        lista_do_wyswietlenia = random.sample(lista_slow, i)
+        print("Wylosowane słowa:", lista_do_wyswietlenia)
+        for j in range(3 * i, 0, -1):
+            print(f"\rPozostały czas {j} s do rozpoczęcia gry", end='', flush=True)
+            time.sleep(1)
+        print("\n" * 15)
+        lista_wpisana = [s.lower() for s in input("Wpisz zapamiętane słowa, oddzielone spacją: ").split()]
+        if (len(lista_wpisana) < len(lista_do_wyswietlenia)):
+            print("Podałeś za mało słów!")
+            break
+        for slowo in lista_wpisana:
+            if slowo in lista_do_wyswietlenia:
+                punkty += 1
+            else:
+                bledy += 1
+            if (bledy > 0):
+                print("Pojawił się błąd!")
+                break
+        i += 1
+    print("Koniec gry!")
+    return punkty
+
 
 
 def tworzenie_listy():
@@ -19,28 +86,34 @@ def tworzenie_listy():
             return lista  # Zwracamy listę tylko jeśli plik istnieje
     except FileNotFoundError:
         print("Nie znaleziono pliku dla tego trybu.")
-        return None
+        tworzenie_listy()
 
 
 def wybor_trybu(lista):
-    print("wybierz tryb gry: \n A <-- gra na czas \n B <-- gra na ilosc \n cofnij")
-    Wybor = input("Wybierz tryb A, B lub cofnij : ")
-
+    print("wybierz tryb gry: \n A <-- gra na czas \n B <-- gra na ilosc \n C <-- gra do pierwszego bledu \n cofnij")
+    Wybor = input("Wybierz tryb A, B, C lub cofnij : ")
     if Wybor == "A":
-        liczba_SEC = int(input("Wybierz ile chcesz miec czasu na zadanie (w sekundach) : "))
-        lista_do_wyswietlenia = random.sample(lista, 5)
-        print("Wylosowane słowa:", lista_do_wyswietlenia)
-        for i in range(liczba_SEC, 0, -1):
-            print(f"\rZniknie za {i} sekund...", end='', flush=True)
-            time.sleep(1)
+        Wynik1 = tryb_gry_na_czas(lista)
+        print("Liczba punktów : ", Wynik1[0],"Liczba błędów: ", Wynik1[1] )
+        # liczba_SEC = int(input("Wybierz ile chcesz miec czasu na zadanie (w sekundach) : "))
+        # lista_do_wyswietlenia = random.sample(lista, 5)
+        # print("Wylosowane słowa:", lista_do_wyswietlenia)
+        # for i in range(liczba_SEC, 0, -1):
+        #     print(f"\rZniknie za {i} sekund...", end='', flush=True)
+        #     time.sleep(1)
 
     elif Wybor == "B":
-        liczba_SLOW = int(input("Wybierz ile słow chcesz wyswietlac do zapamietania : "))
-        lista_do_wyswietlenia = random.sample(lista, liczba_SLOW)
-        print("Wylosowane słowa:", lista_do_wyswietlenia)
-        for i in range(10, 0, -1):
-            print(f"\rZniknie za {i} sekund...", end='', flush=True)
-            time.sleep(1)
+        Wynik2 = tryb_gry_na_ilosc(lista)
+        print("Liczba punktów : ", Wynik2[0], "Liczba błędów: ", Wynik2[1])
+        # liczba_SLOW = int(input("Wybierz ile słow chcesz wyswietlac do zapamietania : "))
+        # lista_do_wyswietlenia = random.sample(lista, liczba_SLOW)
+        # print("Wylosowane słowa:", lista_do_wyswietlenia)
+        # for i in range(10, 0, -1):
+        #     print(f"\rZniknie za {i} sekund...", end='', flush=True)
+        #     time.sleep(1)
+    elif Wybor == "C":
+        Wynik3 = tryb_gry_do_pierwszego_bledu1(lista)
+        print("Liczba punktów : ", Wynik3)
 
     elif Wybor == "cofnij":
         print("Powrot do wyboru poziomu trudnosci")
@@ -49,73 +122,7 @@ def wybor_trybu(lista):
     else:
         print("Nie ma takiego trybu gry")
 
-    print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
-    print("\n" * 10)
-    lista2 = input().split(" ")
-    punkty = 0
-    for slowo in lista2:
-        if slowo in lista_do_wyswietlenia:
-            punkty += 1
-    print(punkty)
-    wybor_trybu(lista)
-
-
-
 def main():
     wybor_trybu(tworzenie_listy())
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-"""import random
-import time
-import os
-
-def tryb_gry_na_czas(lista_slow):
-    czas = int(input("Podaj czas potrzebny na zapamiętanie 10 słów: "))
-    lista_do_wyswietlenia = random.sample(lista_slow, 5)
-    print("Wylosowane słowa:", lista_do_wyswietlenia)
-    for i in range(czas, 0, -1):
-        print(f"\rPozostały czas {i} s do rozpoczęcia gry", end='', flush=True)
-        time.sleep(1)
-   # print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
-    print("\n" * 10)
-    lista_wpisana = input().split(" ")
-    punkty = 0
-    for slowo in lista_slow:
-        if slowo in lista_do_wyswietlenia:
-            punkty += 1
-    return punkty
-
-lista_latwych = ["laptop", "lampa", "telefon", "okno", "zegarek", "butelka", "szalik", "sok", "plecak", "mysz", "garnek", "długopis",
-"nożyczki", "marker", "masło", "ryż", "makaron", "poduszka", "klapki", "gitara", "jabłko", "portfel", "słoik", "czajnik"]
-
-def main():
-    p = tryb_gry_na_czas(lista_latwych)
-    print(p)
-
-if __name__ == "__main__":
-    main()
-    
-    
-    
-#pierwsza wersja:    
-lista_do_wyswietlenia = random.sample(lista_latwych, 5)
-print("Wylosowane słowa:", lista_do_wyswietlenia)
-
-for i in range(3, 0, -1):
-    print(f"\rZniknie za {i} sekund...", end='', flush=True)
-    time.sleep(1)
-
-print("\r" + " " * 30 + "\rCzas minął!", end='', flush=True)
-print("\n" * 10)
-lista_wpisana = input().split(" ")
-punkty = 0
-for slowo in lista2:
-    if slowo in lista_do_wyswietlenia:
-        punkty += 1
-print(punkty)"""
