c9ddd4522678c319e124b8deff78344f1cc5e8e2
Hubert <h.bryki@gmail.com>
2025-06-15 17:36:38 +0200

    tryb czas oraz tryb ilość

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from tkinter import ttk
 import tkinter as tk
+import time
+
 
 class root(tk.Tk):
     def __init__(self, text="Memory słowne"):
@@ -29,10 +31,7 @@ class root(tk.Tk):
 
         statistics_button = ttk.Button(self.frame, text="Statystyki", command= self.statistics_window)
         statistics_button.pack()
-       
-
-        option_button = ttk.Button(self.frame, text="Opcje", command=self.option_window)
-        option_button.pack()
+      
         self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
     def statistics_window(self):
@@ -50,17 +49,138 @@ class root(tk.Tk):
 
         self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-
     def game_window(self):
         self.clear()
+       
 
-    def option_window(self):
+        mode1_button = ttk.Button(self.frame, text="Na Czas ⏰", style= "TButton",command=self.game_choose_time,)
+        mode1_button.pack()
+
+
+        mode2_button = ttk.Button(self.frame, text="Na Ilość 📝", command= self.game_choose_amount)
+        mode2_button.pack()
+       
+        mode3_button = ttk.Button(self.frame, text="Do pierwszego błedu ❌", style= "TButton",command=self.game_mistake,)
+        mode3_button.pack()
+
+        
+
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def game_choose_amount(self):
+        self.clear()
+       
+        self.amount = tk.IntVar()
+
+        entry_label = ttk.Label(self.frame,  text="Wybierz ilość słów", style= "TButton", width= 20)
+        entry_label.pack()
+
+        entry = ttk.Entry(self.frame, textvariable= self.amount, style= "TButton", font=(50), width= 20)
+        entry.pack()
+
+        self.level_list = ["Łatwy", "Łatwy", "Średni", "Trudny"] #Nie wiem czemu musi być tak ale dziąła dobrze xD
+        self.level = tk.StringVar(value="Łatwy")
+        level_menu = ttk.OptionMenu(self.frame, self.level , *self.level_list, style="TButton")
+
+        level_menu.pack()
+
+        ok_button = ttk.Button(self.frame, text="OK", command= self.game_amount)
+        ok_button.pack()
+
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def game_amount(self):
+        self.level = str(self.level.get())
+        self.amount = int(self.amount.get())
+
         self.clear()
 
+        
+        words = ttk.Label(self.frame,  text="tu będą słowa", style= "TButton")
+        words.pack()
+        
+        ok_button = ttk.Button(self.frame, text="OK", command= self.write_words)
+        ok_button.pack()
+
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def game_choose_time(self, time = 5):
+        self.clear()
+        self.time = tk.IntVar()
+
+        entry_label = ttk.Label(self.frame,  text="Wybierz czas w sekundach", style= "TButton")
+        entry_label.pack()
+
+        entry = ttk.Entry(self.frame, textvariable= self.time, style= "TButton", font=(20))
+        entry.pack()
+
+        self.level_list = ["Łatwy", "Łatwy", "Średni", "Trudny"] #Nie wiem czemu musi być tak ale dziąła dobrze xD
+        self.level = tk.StringVar(value="Łatwy")
+        level_menu = ttk.OptionMenu(self.frame, self.level , *self.level_list, style="TButton")
+        level_menu.pack()
+
+        ok_button = ttk.Button(self.frame, text="OK", command= self.game_time)
+        ok_button.pack()
+
+
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def game_time(self):
+        self.clear()
+        self.level = str(self.level.get())
+        time_left = int(self.time.get())
+
+        words = ttk.Label(self.frame,  text="tu będą słowa", style= "TButton")
+        words.pack()
+        time_label = ttk.Label(self.frame, text=str(time_left), style= "TButton")
+        time_label.pack()
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        while True:
+            if time_left <= 0:
+                self.write_words()
+                break
+            else:
+                time_label.config(text= time_left)
+                self.update()
+                time.sleep(1)
+                time_left -= 1
+
+    def game_mistake(self):
+        self.clear()
+        
+ 
+
+    def write_words(self):
+        self.clear()
+        
+        entry_label = ttk.Label(self.frame,  text="Podane słowa", style= "TButton")
+        entry_label.pack()
+
+        self.answer = tk.StringVar()
+
+        entry = ttk.Entry(self.frame, textvariable= self.answer, style= "TButton", font=(20))
+        entry.pack()
+
+        ok_button = ttk.Button(self.frame, text="OK", command= self.game_result)
+        ok_button.pack()
+
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def game_result(self):
+        self.clear()
+        
+        result_label= ttk.Label(self.frame,  text="Wynik", style= "TButton")
+        result_label.pack()
+        
+        back_button = ttk.Button(self.frame, text="Wróć", command=self.menu_window)
+        back_button.pack()
+
+        self.frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
     def clear(self):
         for widget in self.winfo_children():
             widget.destroy()
-
+        self.frame = tk.Frame(self)
+        self.frame.configure(bg="#333446")
 
 class stats_table:
     def __init__(self, root, list, rows, columns):
@@ -77,7 +197,6 @@ class stats_table:
                 
                 self.e.grid(row=i, column=j)
                 
-
 
 if __name__ == "__main__":
     root_ = root()
